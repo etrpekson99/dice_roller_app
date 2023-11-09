628531152a67867e09052a1cfadb3f2e117c3fee
eli <etrpekson1998@gmail.com>
2023-11-09 22:06:36 +0800

    feat: Add gradient background color with a List of Colors

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,21 @@ import 'package:flutter/material.dart';
 void main() {
   // runApp needs a widget tree to run
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        // backgroundColor: Color.fromARGB(255, 82, 163, 203),
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              // this is a List
+              colors: [
+                Color.fromARGB(255, 7, 105, 226),
+                Color.fromARGB(255, 4, 59, 127),
+              ]
+            )
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     ),
